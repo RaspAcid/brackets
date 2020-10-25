5717dd590999c8bb85b8804686df286c011baaad
RaspAcid <rasp.acid@gmail.com>
2020-10-25 13:07:17 +0300

    feat: implement brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  while (true) {
+    let counter = 0;
+    bracketsConfig.forEach(item => {
+        if (str.includes(item[0] + item[1])) {
+            str = str.replace(item[0] + item[1], '');
+            counter += 1;
+        }
+    })
+    if (counter === 0) break;
+}
+
+return (str === '') ? true : false;
 }
